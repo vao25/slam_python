3158c76e9591b002a4776295a129a224bd86c25a
vao25 <115639120+vao25@users.noreply.github.com>
2023-02-28 21:31:08 +0100

    Add files via upload

--- ekfslam_v1/vehicle_model.py
+++ ekfslam_v1/vehicle_model.py
@@ -2,6 +2,18 @@ import numpy as np
 from pi_to_pi import pi_to_pi
 
 def vehicle_model(xv, V, G, WB, dt):
+"""
+ INPUTS:
+   xv - vehicle pose [x;y;phi]
+   V - velocity
+   G - steer angle (gamma)
+   WB - wheelbase
+   dt - change in time
+
+ OUTPUTS:
+   xv - new vehicle pose
+
+"""
     xv = np.array([xv[0] + V*dt*np.cos(G+xv[2]), 
                    xv[1] + V*dt*np.sin(G+xv[2]),
                    pi_to_pi(xv[2] + V*dt*np.sin(G)/WB)])
