200fcba564074a2305f48311eb09f1c75f4a8033
vao25 <115639120+vao25@users.noreply.github.com>
2023-03-07 22:21:16 +0100

    Add files via upload

--- ekfslam_v1/ekfslam_sim.py
+++ ekfslam_v1/ekfslam_sim.py
@@ -94,4 +94,40 @@ def ekfslam_sim(lm, wp):
     
         # offline data store
         data= store_data(data, x, P, xtrue)
+    data= finalise_data(data);
+    
+    
+def initialise_store(x, P, xtrue):
+    # offline storage initialisation
+    data = {}
+    data['i'] = 1
+    data['path'] = x
+    data['true'] = xtrue
+    data['state'] = [{}]
+    data['state'][0]['x'] = x
+    #data['state'][0]['P'] = P
+    data['state'][0]['P'] = np.diag(P)
+    return data
+
+def store_data(data, x, P, xtrue):
+    # add current data to offline storage
+    CHUNK = 5000
+    if data['i'] == data['path'].shape[1]: # grow array in chunks to amortise reallocation
+        data['path'] = np.hstack((data['path'], np.zeros((3, CHUNK))))
+        data['true'] = np.hstack((data['true'], np.zeros((3, CHUNK))))
+    i = data['i'] + 1
+    data['i'] = i
+    data['path'][:, i-1] = x[0:3]
+    data['true'][:, i-1] = xtrue
+    data['state'].append({})
+    data['state'][i-1]['x'] = x
+    #data['state'][i-1]['P'] = P
+    data['state'][i-1]['P'] = np.diag(P)
+    return data
+
+def finalise_data(data):
+    # offline storage finalisation
+    data['path'] = data['path'][:, 0:data['i']]
+    data['true'] = data['true'][:, 0:data['i']]
+    return data
     
