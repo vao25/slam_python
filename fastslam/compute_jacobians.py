31b5525e9e6a2084188e88d4b3f8dc9f25ad710b
vao25 <115639120+vao25@users.noreply.github.com>
2023-03-23 22:15:53 +0100

    Add files via upload

---
+++ fastslam/compute_jacobians.py
@@ -0,0 +1,28 @@
+import numpy as np
+from pi_to_pi import pi_to_pi
+
+def compute_jacobians(particle, idf, R):
+    xv = particle.xv
+    xf = particle.xf[:,idf]
+    Pf = particle.Pf[:,:,idf]
+    
+    zp = np.zeros((2,len(idf)))
+    Hv = np.zeros((2,3,len(idf)))
+    Hf = np.zeros((2,2,len(idf)))
+    Sf = np.zeros((2,2,len(idf)))
+    
+    for i in range(len(idf)):
+        dx = xf[0,i] - xv[0]
+        dy = xf[1,i] - xv[1]
+        d2 = dx**2 + dy**2
+        d = np.sqrt(d2)
+        
+        zp[:,i] = np.array([d, pi_to_pi(np.arctan2(dy,dx) - xv[2])]) # predicted observation
+        
+        Hv[:,:,i] = np.array([[-dx/d, -dy/d, 0], # Jacobian wrt vehicle states
+                              [dy/d2, -dx/d2, -1]])
+        Hf[:,:,i] = np.array([[dx/d, dy/d], # Jacobian wrt feature states
+                              [-dy/d2, dx/d2]])
+        Sf[:,:,i] = np.dot(Hf[:,:,i], np.dot(Pf[:,:,i], Hf[:,:,i].T)) + R # innovation covariance of 'feature observation given the vehicle'
+        
+    return zp, Hv, Hf, Sf
