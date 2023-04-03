febb70cd173839ada02ce42b47763186602f6091
vao25 <115639120+vao25@users.noreply.github.com>
2023-04-03 21:35:46 +0200

    Add files via upload

--- ekfslam_v1/ekfslam_sim.py
+++ ekfslam_v1/ekfslam_sim.py
@@ -104,15 +104,21 @@ def initialise_store(x, P, xtrue):
     # offline storage initialisation
     data = {}
     data['i'] = 1
-    data['path'] = x
-    data['true'] = xtrue
+    data['path'] = np.zeros((3, 1))
+    data['path'][0,0] = x[0]
+    data['path'][1,0] = x[1]
+    data['path'][2,0] = x[2]
+    data['true'] = np.zeros((3, 1))
+    data['true'][0,0] = xtrue[0]
+    data['true'][1,0] = xtrue[1]
+    data['true'][2,0] = xtrue[2]
     data['state'] = [{}]
-    data['state'][0]['x'] = x
+    data['state'][0]['x'] = np.copy(x)
     #data['state'][0]['P'] = P
-    data['state'][0]['P'] = np.diag(P)
+    data['state'][0]['P'] = np.diag(np.copy(P))
     return data
 
-def store_data(data, x, P, xtrue):
+def store_data(data, x, P, xtrue):    
     # add current data to offline storage
     CHUNK = 5000
     if data['i'] == data['path'].shape[1]: # grow array in chunks to amortise reallocation
@@ -120,10 +126,14 @@ def store_data(data, x, P, xtrue):
         data['true'] = np.hstack((data['true'], np.zeros((3, CHUNK))))
     i = data['i'] + 1
     data['i'] = i
-    data['path'][:, i-1] = x[0:3]
-    data['true'][:, i-1] = xtrue
+    data['path'][0,i-1] = x[0]
+    data['path'][1,i-1] = x[1]
+    data['path'][2,i-1] = x[2]
+    data['true'][0,i-1] = xtrue[0]
+    data['true'][1,i-1] = xtrue[1]
+    data['true'][2,i-1] = xtrue[2]
     data['state'].append({})
-    data['state'][i-1]['x'] = x
+    data['state'][i-1]['x'] = np.copy(x)
     #data['state'][i-1]['P'] = P
     data['state'][i-1]['P'] = np.diag(P)
     return data
