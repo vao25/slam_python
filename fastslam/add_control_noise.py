89c1e2da77f0ba2da800a47b6377570132add247
vao25 <115639120+vao25@users.noreply.github.com>
2023-04-03 12:48:15 +0200

    Add files via upload

--- fastslam/add_control_noise.py
+++ fastslam/add_control_noise.py
@@ -4,7 +4,7 @@ from multivariate_gauss import multivariate_gauss
 def add_control_noise(V,G,Q, addnoise):
     # Add random noise to nominal control values
     if addnoise == 1:
-        C= multivariate_gauss(np.array([V,G]),Q, 1) # if Q might be correlated
+        C= multivariate_gauss(np.array([[V],[G]]),Q, 1) # if Q might be correlated
         V= C[0]
         G= C[1]
     return V,G
