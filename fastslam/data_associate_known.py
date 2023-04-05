f672f97dd723ca0c8bce7667d3f1cee46d8a9d80
vao25 <115639120+vao25@users.noreply.github.com>
2023-04-05 21:41:33 +0200

    Add files via upload

--- fastslam/data_associate_known.py
+++ fastslam/data_associate_known.py
@@ -4,7 +4,7 @@ def data_associate_known(z, idz, table, Nf):
     # [zf,idf,zn, table]= data_associate_known(z, idz, table, Nf)
     # For simulations with known data-associations, this function maintains a feature/observation lookup table. It returns the updated table, the set of associated observations and the set of observations to new features.
     
-    zf = np.array([])
+    zf = np.array([[],[]])
     idf = np.array([])
     zn = np.array([[],[]])
     idn = np.array([])
@@ -13,14 +13,15 @@ def data_associate_known(z, idz, table, Nf):
     for i in range(len(idz)):
         ii = idz[i]
         if table[0,ii] == -1: # new feature
-            np.append(zn, [[z[0,i]], [z[1,i]]], axis = 1)
-            np.append(idn, ii)
+            zn = np.append(zn, [[z[0,i]], [z[1,i]]], axis = 1)
+            idn = np.append(idn, ii)
         else:
-            np.append(zf, z[:,i], axis = 1)
-            np.append(idf, table[0,ii], axis = 1)
+            zf = np.append(zf, [[z[0,i]], [z[1,i]]], axis = 1)
+            idf = np.append(idf, table[0,ii])
     
     if idn:
         # add new feature IDs to lookup table
-        table[0,idn] = Nf + np.arange(zn.shape[1]) # add new feature positions to lookup table
+        for j in range(zn.shape[1]):
+            table[0,idn[j]] = Nf  + j # add new feature positions to lookup table
     return zf, idf, zn, table
 
