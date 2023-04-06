c77d926b31b8e85e014287d75c1744af97dbb9e1
vao25 <115639120+vao25@users.noreply.github.com>
2023-04-06 16:19:43 +0200

    Add files via upload

--- fastslam/feature_update.py
+++ fastslam/feature_update.py
@@ -7,12 +7,13 @@ def feature_update(particle, z, idf, R):
     # Having selected a new pose from the proposal distribution, this pose is assumed
     # perfect and each feature update may be computed independently and without pose uncertainty.
     
-    xf = particle.xf[:,idf]
-    Pf = particle.Pf[:,:,idf]
+    if idf.size != 0:
+        xf = particle.xf[:,idf]
+        Pf = particle.Pf[:,:,idf]
     
-    zp, Hv, Hf, Sf = compute_jacobians(particle, idf, R)
-    v = z - zp
-    v[1,:] = pi_to_pi(v[1,:])
+        zp, Hv, Hf, Sf = compute_jacobians(particle, idf, R)
+        v = z - zp
+        v[1,:] = pi_to_pi(v[1,:])
     
     for i in range(len(idf)):
         vi = v[:,i]
@@ -22,7 +23,8 @@ def feature_update(particle, z, idf, R):
         
         xf[:,i], Pf[:,:,i] = KF_cholesky_update(xfi, Pfi, vi, R, Hfi)
         
-    particle.xf[:,idf] = xf
-    particle.Pf[:,:,idf] = Pf
+    if idf.size != 0:
+        particle.xf[:,idf] = xf
+        particle.Pf[:,:,idf] = Pf
     
     return particle 
