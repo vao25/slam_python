2c6c75ceaf2a838990639ea928175da466d51741
vao25 <115639120+vao25@users.noreply.github.com>
2023-04-06 22:52:13 +0200

    Add files via upload

--- ekfslam_v1/add_observation_noise.py
+++ ekfslam_v1/add_observation_noise.py
@@ -5,10 +5,10 @@ def add_observation_noise(z,R, addnoise):
     # Add random measurement noise. We assume R is diagonal.
     
     if addnoise == 1:
-        len= z.shape[1]
-        if len > 0:
-            z[0,:]= z[0,:] + np.random.randn(1,len)*np.sqrt(R[0,0])
-            z[1,:]= z[1,:] + np.random.randn(1,len)*np.sqrt(R[1,1])
+        l= z.shape[1]
+        if l > 0:
+            z[0,:]= z[0,:] + np.random.randn(1,l)*np.sqrt(R[0,0])
+            z[1,:]= z[1,:] + np.random.randn(1,l)*np.sqrt(R[1,1])
     return z
 
 
