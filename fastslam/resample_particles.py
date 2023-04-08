0d71f9bc08a15c4839ee2725d801fdf640980ef4
vao25 <115639120+vao25@users.noreply.github.com>
2023-04-08 17:39:09 +0200

    Add files via upload

--- fastslam/resample_particles.py
+++ fastslam/resample_particles.py
@@ -1,5 +1,6 @@
 import numpy as np
 from stratified_resample import stratified_resample
+import copy
 
 def resample_particles(particles, Nmin, doresample):
     # particles= resample_particles(particles, Nmin, doresample)
@@ -16,8 +17,11 @@ def resample_particles(particles, Nmin, doresample):
     
     keep, Neff = stratified_resample(w)
     if Neff < Nmin and doresample == 1:
+        particles2 = []
         for i in range(N):
-            particles[i] = particles[keep[i]]
+            particles2.append(copy.deepcopy([particles[keep[i]]])[0])
         for i in range(N):
-            particles[i].w = 1/N
+            particles2[i].w = 1/N
+        return particles2
+    
     return particles
