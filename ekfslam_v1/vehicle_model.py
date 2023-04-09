e5e9dbb8a0bad37391a27edafff560b87a8c4f9f
vao25 <115639120+vao25@users.noreply.github.com>
2023-04-09 13:56:24 +0200

    Add files via upload

--- ekfslam_v1/vehicle_model.py
+++ ekfslam_v1/vehicle_model.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import sin, cos
 from pi_to_pi import pi_to_pi
 
 def vehicle_model(xv, V, G, WB, dt):
@@ -14,7 +15,7 @@ def vehicle_model(xv, V, G, WB, dt):
        xv - new vehicle pose
     """
     
-    xv = np.array([xv[0] + V*dt*np.cos(G+xv[2]), 
-                   xv[1] + V*dt*np.sin(G+xv[2]),
-                   pi_to_pi(xv[2] + V*dt*np.sin(G)/WB)])
+    xv = np.array([[xv[0][0] + V*dt*cos(G+xv[2][0])], 
+                   [xv[1][0] + V*dt*sin(G+xv[2][0])],
+                   pi_to_pi(xv[2] + V*dt*sin(G)/WB)])
     return xv
