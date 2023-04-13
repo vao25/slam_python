8949bd64e8d42a84ccd8b1dc577f8aab737b0782
vao25 <115639120+vao25@users.noreply.github.com>
2023-04-13 21:38:15 +0200

    Add files via upload

--- ukfslam/compute_steering.py
+++ ukfslam/compute_steering.py
@@ -22,7 +22,7 @@ def compute_steering(x, wp, iwp, minD, G, rateG, maxG, dt):
     
     # determine if current waypoint reached
     cwp = np.copy(wp[:,iwp])
-    d2 = (cwp[0]-x[0])**2 + (cwp[1]-x[1])**2
+    d2 = (cwp[0]-x[0][0])**2 + (cwp[1]-x[1][0])**2
     if d2 < minD**2:
         iwp = iwp+1 # switch to next
         if iwp >= wp.shape[1]: # reached final waypoint, flag and return
@@ -30,7 +30,7 @@ def compute_steering(x, wp, iwp, minD, G, rateG, maxG, dt):
             return G, iwp
         cwp = np.copy(wp[:,iwp]) # next waypoint
     # compute change in G to point towards current waypoint
-    deltaG = pi_to_pi(math.atan2(cwp[1]-x[1], cwp[0]-x[0]) - x[2] - G)
+    deltaG = pi_to_pi(math.atan2(cwp[1]-x[1][0], cwp[0]-x[0][0]) - x[2] - G)
     # limit rate
     maxDelta = rateG*dt
     if abs(deltaG) > maxDelta:
