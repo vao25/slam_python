1e169699a38ea8081cb956c91c121d0342397f61
vao25 <115639120+vao25@users.noreply.github.com>
2023-04-15 13:10:28 +0200

    Add files via upload

--- ukfslam/sqrt_posdef.py
+++ ukfslam/sqrt_posdef.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 def sqrt_posdef(P):
+    # R = sqrt_posdef(P, type)
+    # INPUTS: P - symmetric positive definite matrix
+    # OUTPUT: R - square-root of P, such that P = R*R'
+    # Compute the square-root of a symmetric positive definite matrix.
+    
     # cholesky decomposition (triangular), P = R*R'
     R = np.transpose(np.linalg.cholesky(P))
     return R 
