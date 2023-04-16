36f45567be044f0eb9849713c4f19cbebca0fcaf
vao25 <115639120+vao25@users.noreply.github.com>
2023-04-16 20:45:24 +0200

    Add files via upload

--- ukfslam/update.py
+++ ukfslam/update.py
@@ -5,7 +5,7 @@ def update(z,R,idf):
     global XX
     global PX
     for i in range(len(idf)):
-        XX,PX = unscented_update(observe_model, observediff, XX,PX, z[:,i],R, idf[i])
+        XX,PX = unscented_update(observe_model, observediff, XX,PX, z[:,[i]],R, idf[i])
 
 def observediff(z1, z2):
     dz = z1-z2
